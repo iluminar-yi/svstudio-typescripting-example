da4b28952da3de360c541c9b967757f63ad9b03b
Iluminar <yi.iluminar@gmail.com>
2020-07-17 01:08:36 -0700

    Commit example code

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,32 @@ import { FrameworkEnvironment, SVScript, SVScriptFactory } from '../framework/ty
 import packageJsonClientInfo from './package-json-client-info';
 
 const svScriptFactory: SVScriptFactory = ({ SV, log }: FrameworkEnvironment): SVScript => {
+  let count = 0;
+  const intervalCallback = (): void => {
+    log.info('Callback called', count);
+    count++;
+    const navMain = SV.getMainEditor().getNavigation();
+    const [tLeft] = navMain.getTimeViewRange();
+    navMain.setTimeLeft(tLeft + SV.QUARTER / count);
+
+    if (count < 100) {
+      SV.setTimeout(500, intervalCallback);
+    } else {
+      SV.finish();
+    }
+  };
+
+  const main = (): void => {
+    const navMain = SV.getMainEditor().getNavigation();
+
+    navMain.setTimeScale(50 / SV.QUARTER);
+
+    intervalCallback();
+  };
+
   return {
     getClientInfo: packageJsonClientInfo,
-    main(): void {
-      log.info('Hello world!');
-      SV.finish();
-    },
+    main,
   };
 };
 
